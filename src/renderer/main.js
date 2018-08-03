5bbc8867e1138f2829c058a337ab8eb5294ba8af
prokosna <prokosna@gmail.com>
2018-08-03 20:48:34 +0900

    Initial commit

---
+++ src/renderer/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import 'bootswatch/dist/darkly/bootstrap.min.css'
+
+library.add(faCoffee)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
